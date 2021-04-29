585ffcdb0123e0569c84a3c2990144c2ddf643f9
Cody Fagley <cfagley@localhost.localdomain>
2021-04-29 17:00:21 -0600

    Added MouseVision to my Website

--- js/projects.js
+++ js/projects.js
@@ -8,6 +8,7 @@
   Controls Dynamic Functionality for Project Showcases
 */
 
+var lock = false;
 
 $( function() {
   $( "#dialog" ).dialog({
@@ -26,18 +27,89 @@ $( function() {
     open: function (event, ui) {
       $('#dialog').bind('click', function () {
           $("#dialog").dialog('close');
+          lock = false;
       });
+      lock = true;
     }
   });
+});
 
+$( function() {
+  $( "#dialog2" ).dialog({
+    autoOpen: false,
+    dialogClass: "no-close",
+    modal: true,
+    width: 650,
+    show: {
+      effect: "fade",
+      duration: 500
+    },
+    hide: {
+      effect: "fade",
+      duration: 500
+    },
+    open: function (event, ui) {
+      $('#dialog2').bind('click', function () {
+          $("#dialog2").dialog('close');
+          lock = false;
+      });
+      lock = true;
+    }
+  });
 });
 
 $("document").ready(function(){
 
+  activateProject(0);
 
   $("#xita_src").on( "click", function(){
+    if (!lock)
     $( "#dialog" ).dialog( "open" );
   });
+  $("#xita").on( "click", function(){
+    if (!lock)
+    $( "#dialog2" ).dialog( "open" );
+  });
 
+  $('#dot_xita').on("click", function(){
+    activateProject(0);
+  })
+
+  $('#dot_mousevision').on("click", function(){
+    activateProject(1);
+  })
 
 });
+
+
+var activated_project = -1;
+
+
+function activateProject(id)
+{
+  if (id >= 0)
+  {
+    switch(activated_project)
+    {
+      case 0: 
+        $("#xita").css("opacity", "0");
+        break;
+      case 1: 
+        $("#mouse_vision").css("opacity", "0");
+        break;
+      default:
+    }
+    activated_project = id;
+    switch(id)
+    {
+      case 0:
+        $("#xita").css("opacity", "100%");
+        break;
+      case 1:
+        $("#mouse_vision").css("opacity", "100%");
+        break;
+
+      default:
+    }
+  }
+}
