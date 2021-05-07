5e463fb6cc25a146dbc57c04ba4d12511df78e35
Cody Fagley <cfagley@localhost.localdomain>
2021-05-07 15:02:48 -0600

    Fixing glitches on mobile

--- js/experience.js
+++ js/experience.js
@@ -12,10 +12,48 @@
 $("document").ready(function()
 {
 
-  
+  activateExperience(0);
+
+  $("#dot_codeus").on("click", function(){
+    activateExperience(0);
+  });
+  $("#dot_teton").on("click", function(){
+    activateExperience(1);
+  });
 
 
 });
 
 
+activatedExperience = -1;
+front = 100;
+
+function activateExperience(id)
+{
+  switch (activatedExperience)
+  {
+    case 0:
+      $("#codeus").css("opacity", 0);
+      $("#dot_codeus").css("border-color", "black");
+      break;
+    case 1:
+      $("#teton").css("opacity", 0);
+      $("#dot_teton").css("border-color", "black");
+      break;
+  }
+  activatedExperience = id;
+  switch(id)
+  {
+    case 0:
+      $("#codeus").css("opacity", "100%");
+      $("#codeus").css("z-index", ++front);
+      $("#dot_codeus").css("border-color", "teal");
+      break;
+    case 1:
+      $("#teton").css("opacity", "100%");
+      $("#teton").css("z-index", ++front);
+      $("#dot_teton").css("border-color", "teal");
+      break;
+  }
+}
 
