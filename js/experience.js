6e4444266afe1232f0ae766ee847c2aa4d6400a1
Cody Fagley <cfagley@localhost.localdomain>
2021-05-10 17:21:23 -0600

    Added 2 experience showcases; generally improved aesthetics

--- js/experience.js
+++ js/experience.js
@@ -20,6 +20,12 @@ $("document").ready(function()
   $("#dot_teton").on("click", function(){
     activateExperience(1);
   });
+  $("#dot_siteseers").on("click", function(){
+    activateExperience(2);
+  });
+  $("#dot_westmoreland").on("click", function(){
+    activateExperience(3);
+  });
 
 
 });
@@ -40,6 +46,14 @@ function activateExperience(id)
       $("#teton").css("opacity", 0);
       $("#dot_teton").css("border-color", "black");
       break;
+    case 2:
+      $("#siteseers").css("opacity", 0);
+      $("#dot_siteseers").css("border-color", "black");
+      break;
+    case 3:
+      $("#westmoreland").css("opacity", 0);
+      $("#dot_westmoreland").css("border-color", "black");
+      break;
   }
   activatedExperience = id;
   switch(id)
@@ -53,6 +67,16 @@ function activateExperience(id)
       $("#teton").css("opacity", "100%");
       $("#teton").css("z-index", ++front);
       $("#dot_teton").css("border-color", "teal");
+      break;
+    case 2:
+      $("#siteseers").css("opacity", "100%");
+      $("#siteseers").css("z-index", ++front);
+      $("#dot_siteseers").css("border-color", "teal");
+      break;
+    case 3:
+      $("#westmoreland").css("opacity", "100%");
+      $("#westmoreland").css("z-index", ++front);
+      $("#dot_westmoreland").css("border-color", "teal");
       break;
   }
 }
